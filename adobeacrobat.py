f04cc9221a2311ec2e60dae7a52bb95ff59cc6b6
Tofuine <73127480+realtofuine@users.noreply.github.com>
2022-06-25 17:03:06 -0500

    modified files to show app name in discord status

---
+++ adobeacrobat.py
@@ -0,0 +1,23 @@
+from pypresence import Presence
+import time
+import psutil
+import os
+
+start = int(time.time())
+client_id = "990356931321413712" #application's client id; you only need to modify this to add images to rich presence
+RPC = Presence(client_id)
+RPC.connect()
+
+while ("Acrobat.exe" in (i.name() for i in psutil.process_iter())): #keep repeating updating rich presence
+    RPC.update(
+        large_image = "acrobat-reader", #name of your asset in discord's developer portal
+        large_text = "Adobe Acrobat DC",
+        details = "Adobe Acrobat DC",
+        state = "Reading a PDF",
+        start = start,
+        buttons = [{"label": "Hello", "url": "https://google.com"}, {"label": "Discord", "url": "https://discord.gg/"}] #up to 2 buttons allowed
+    )
+    time.sleep(15) #can be as low as 15, depends on how often you want to update rich presence
+
+RPC.clear()
+os.system("python appmanager.py") #restart appmanager.py if Adobe Acrobat is not running
---
+++ appmanager.py
@@ -0,0 +1,13 @@
+from pypresence import Presence
+import time
+import psutil
+import os
+
+while True: #keep repeating updating rich presence
+    if ("xournalpp.exe" in (i.name() for i in psutil.process_iter())): #if Xournal++ is running
+        os.system("python xournal.py")
+    elif ("Acrobat.exe" in (i.name() for i in psutil.process_iter())): #if Adobe Acrobat is running
+        os.system("python adobeacrobat.py")
+    else:
+        time.sleep(15)
+        
