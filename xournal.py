ddf20aa5765c8c548d24881a98e097bcf29bdca0
Tofuine <73127480+realtofuine@users.noreply.github.com>
2022-07-03 19:07:45 -0500

    add Idle details

--- xournal.py
+++ xournal.py
@@ -2,6 +2,20 @@ from pypresence import Presence
 import time
 import psutil
 import os
+from ctypes import Structure, windll, c_uint, sizeof, byref
+
+class LASTINPUTINFO(Structure):
+    _fields_ = [
+        ('cbSize', c_uint),
+        ('dwTime', c_uint),
+    ]
+
+def get_idle_duration():
+    lastInputInfo = LASTINPUTINFO()
+    lastInputInfo.cbSize = sizeof(lastInputInfo)
+    windll.user32.GetLastInputInfo(byref(lastInputInfo))
+    millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+    return millis / 1000.0
 
 start = int(time.time())
 client_id = "990311942986010694" #xournal's client id
@@ -9,7 +23,17 @@ RPC = Presence(client_id)
 RPC.connect()
 
 while ("xournalpp.exe" in (i.name() for i in psutil.process_iter())): #keep repeating updating rich presence
-    RPC.update(
+    get_idle_duration()
+    if(get_idle_duration() > 60): #if the user is idle for more than 1 minute, update the state to "Idle"
+        RPC.update(
+            large_image = "favicon-1",
+            large_text = "Xournal++",
+            details = "Editing a file",
+            state = "Idle",
+            start = start,
+        )
+    else:
+        RPC.update(
         large_image = "favicon-1", #name of your asset in discord's developer portal
         large_text = "Xournal++",
         details = "Editing a file",
@@ -18,6 +42,7 @@ while ("xournalpp.exe" in (i.name() for i in psutil.process_iter())): #keep repe
         #if you want meaningful buttons, uncomment the below line
         # buttons = [{"label": "Hello", "url": "https://google.com"}, {"label": "Discord", "url": "https://discord.gg/"}] #up to 2 buttons allowed
     )
+
     time.sleep(15) #can be as low as 15, depends on how often you want to update rich presence
 
 RPC.clear()
--- zoom.py
+++ zoom.py
@@ -2,6 +2,20 @@ from pypresence import Presence
 import time
 import psutil
 import os
+from ctypes import Structure, windll, c_uint, sizeof, byref
+
+class LASTINPUTINFO(Structure):
+    _fields_ = [
+        ('cbSize', c_uint),
+        ('dwTime', c_uint),
+    ]
+
+def get_idle_duration():
+    lastInputInfo = LASTINPUTINFO()
+    lastInputInfo.cbSize = sizeof(lastInputInfo)
+    windll.user32.GetLastInputInfo(byref(lastInputInfo))
+    millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+    return millis / 1000.0
 
 start = int(time.time())
 client_id = "990831184512679946"
@@ -9,12 +23,22 @@ RPC = Presence(client_id)
 RPC.connect()
 
 while ("Zoom.exe" in (i.name() for i in psutil.process_iter())):
-    RPC.update(
-        large_image = "zoom",
-        large_text = "Zoom Meetings",
-        details = "Zoom Meetings",
-        state = "In a Zoom Meeting",
-        start = start,
+    get_idle_duration()
+    if(get_idle_duration() > 300):
+        RPC.update(
+            large_image = "zoom",
+            large_text = "Zoom Meetings",
+            details = "Zoom Meetings",
+            state = "Idle",
+            start = start,
+        )
+    else:
+        RPC.update(
+            large_image = "zoom",
+            large_text = "Zoom Meetings",
+            details = "Zoom Meetings",
+            state = "In a Zoom Meeting",
+            start = start,
     )
     time.sleep(15)
 
